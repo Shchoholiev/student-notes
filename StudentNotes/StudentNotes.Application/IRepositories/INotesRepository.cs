abdfe0e969b6ba16361c445716fb2bca6b32a6ef
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-08 21:02:59 +0300

    Added Interfaces for repositories.

---
+++ StudentNotes/StudentNotes.Application/IRepositories/INotesRepository.cs
@@ -0,0 +1,19 @@
+﻿using StudentNotes.Core.Entities.Notes;
+
+namespace StudentNotes.Application.IRepositories
+{
+    public interface INotesRepository
+    {
+        Task AddAsync(NoteBase item);
+
+        Task UpdateAsync(NoteBase item);
+
+        Task DeleteAsync(NoteBase item);
+
+        Task<NoteBase> GetNoteAsync(int id);
+
+        Task<IEnumerable<NoteBase>> GetAllAsync(int month);
+
+        Task<IEnumerable<NoteBase>> GetAllAsync(DateOnly day);
+    }
+}
