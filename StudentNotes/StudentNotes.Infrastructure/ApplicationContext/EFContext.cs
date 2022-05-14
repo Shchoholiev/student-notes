46cea3c50ff10eef47f3fc42da11e2a053fabe5d
Михаил Белодед <mbeloded200@gmail.com>
2022-05-14 18:24:34 +0300

    Added Db sets.

--- StudentNotes/StudentNotes.Infrastructure/ApplicationContext/EFContext.cs
+++ StudentNotes/StudentNotes.Infrastructure/ApplicationContext/EFContext.cs
@@ -1,8 +1,38 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StudentNotes.Core.Entities;
+using StudentNotes.Core.Entities.Identity;
+using StudentNotes.Core.Entities.Notes;
 
 namespace StudentNotes.Infrastructure.ApplicationContext
 {
     public class EFContext
     {
+
+        public EFContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<UserToken> UserTokens { get; set; }
+
+        public DbSet<FileNote> FileNotes { get; set; }
+
+        public DbSet<TextNote> TextNotes { get; set; }
+
+        public DbSet<NoteBase> NoteBases { get; set; }
+
+        public DbSet<Type> Types { get; set; }
+
+        public DbSet<Teacher> Teachers { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<File> Files { get; set; }
     }
 }
