fc45acd44fea593f07203d5d7f5f4b0d65ff7f32
Михаил Белодед <mbeloded200@gmail.com>
2022-05-16 00:03:14 +0300

    Repositories' interfaces was implemented.

--- StudentNotes/StudentNotes.Infrastructure/Repositories/GenericRepository.cs
+++ StudentNotes/StudentNotes.Infrastructure/Repositories/GenericRepository.cs
@@ -1,8 +1,70 @@
-﻿
+﻿using StudentNotes.Application.IRepositories;
+using StudentNotes.Application.Paging;
+using StudentNotes.Core.Entities;
+using System.Linq.Expressions;
 
 namespace StudentNotes.Infrastructure.Repositories
 {
-    internal class GenericRepository
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : EntityBase
     {
+        public Task AddAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attach(params object[] obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetOneAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetOneAsync(int id, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters, Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- StudentNotes/StudentNotes.Infrastructure/Repositories/NotesRepository.cs
+++ StudentNotes/StudentNotes.Infrastructure/Repositories/NotesRepository.cs
@@ -1,8 +1,38 @@
-﻿
+﻿using StudentNotes.Application.IRepositories;
+using StudentNotes.Core.Entities.Notes;
 
 namespace StudentNotes.Infrastructure.Repositories
 {
-    internal class NotesRepository
+    public class NotesRepository : INotesRepository
     {
+        public Task AddAsync(NoteBase item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(NoteBase item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<NoteBase>> GetAllAsync(int month)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<NoteBase>> GetAllAsync(DateOnly day)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteBase> GetNoteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(NoteBase item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
