d2e330013150393ff912866c30e99e02236d692b
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-21 17:21:56 +0300

    Added controller

---
+++ StudentNotes/StudentNotes.API/Controllers/TeachersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentNotes.Application.IRepositories;
+using StudentNotes.Core.Entities;
+
+namespace StudentNotes.API.Controllers
+{
+    public class TeachersController : Controller
+    {
+        private readonly IGenericRepository<Teacher> _teachersRepository;
+
+        public TeachersController(IGenericRepository<Teacher> teachersRepository)
+        {
+            this._teachersRepository = teachersRepository;
+        }
+    }
+}
