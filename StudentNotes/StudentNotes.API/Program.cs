14708dc696fa7086be08dcfd540b7c84a2adc91b
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-21 13:26:33 +0300

    Configured Middleware.

--- StudentNotes/StudentNotes.API/Program.cs
+++ StudentNotes/StudentNotes.API/Program.cs
@@ -1,13 +1,37 @@
+using StudentNotes.API;
+using StudentNotes.Infrastructure;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddJWTTokenServices(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("allowMyOrigin",
+    builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .WithExposedHeaders("X-Pagination");
+    });
+});
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddServices();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//var scope = app.Services.CreateScope();
+//var context = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+//await DbInitializer.Initialize(context);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,9 +40,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
+app.UseCors("allowMyOrigin");
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
