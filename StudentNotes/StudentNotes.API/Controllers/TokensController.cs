16cdc96a7f2f083f97c4811d02768e3238252450
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-21 12:39:56 +0300

    Added Controller

---
+++ StudentNotes/StudentNotes.API/Controllers/TokensController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentNotes.Application.IServices;
+
+namespace StudentNotes.API.Controllers
+{
+    public class TokensController : Controller
+    {
+        private readonly ITokenService _tokenService;
+
+        private readonly IUsersService _usersService;
+
+        public TokensController(ITokenService tokenService, IUsersService usersService)
+        {
+            this._tokenService = tokenService;
+            this._usersService = usersService;
+        }
+    }
+}
