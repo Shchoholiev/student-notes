3fccd40872041ce27541de347d77e0a387f4cb11
Михаил Белодед <mbeloded200@gmail.com>
2022-05-22 13:22:53 +0300

    Notes repository was fixed.

--- StudentNotes/StudentNotes.API/Controllers/NotesController.cs
+++ StudentNotes/StudentNotes.API/Controllers/NotesController.cs
@@ -1,6 +1,49 @@
-﻿namespace StudentNotes.API.Controllers
+﻿using StudentNotes.Application.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+using StudentNotes.Core.Entities.Notes;
+
+namespace StudentNotes.API.Controllers
 {
-    public class NotesController
+    //[Authorize]
+    [ApiController]
+    [Route("api/notes")]
+    public class NotesController : Controller
     {
+        private readonly INotesRepository _notesRepository;
+
+        public NotesController(INotesRepository notesRepository)
+        {
+            this._notesRepository = notesRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NoteBase>>> GetNotesByDay([FromQuery] DateOnly day)
+        {
+            return await this._notesRepository.GetDayNotesAsync(day);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteBase>> GetNote(int id)
+        {
+            var note = await this._notesRepository.GetNoteAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return note;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var note = await this._notesRepository.GetNoteAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            await this._notesRepository.DeleteAsync(note);
+
+            return NoContent();
+        }
     }
 }
